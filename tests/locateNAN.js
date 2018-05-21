464c80e02cea0498cc1a7ee7068c897a440a39bb
Damian Krzeminski <pirxpilot@code42day.com>
2018-05-20 19:55:43 -0600

    add support for locating napi.h header

--- tests/locateNAN.js
+++ tests/locateNAN.js
@@ -1,6 +1,6 @@
 "use strict";
 
-let locateNAN = require("..").locateNAN;
+let locateIncludes = require("..").locateNAN;
 let path = require("path");
 let assert = require("assert");
 
@@ -15,25 +15,32 @@ fixtures/project
 
 */
 
-describe("locateNAN", function () {
+describe("locateIncludes", function () {
     const PROJECT_DIR = path.resolve(__dirname, "fixtures", "project");
     const NAN_DIR = path.join(PROJECT_DIR, "node_modules", "nan");
+    const NAPI_DIR = path.join(PROJECT_DIR, "node_modules", "node-addon-api");
 
     it("should locate NAN from dependency", function () {
         let dir = path.join(PROJECT_DIR, "node_modules", "dep-1");
-        let nan = locateNAN(dir);
+        let nan = locateIncludes(dir, "nan", "nan.h");
         assert.equal(nan, NAN_DIR);
     });
 
     it("should locate NAN from nested dependency", function () {
         let dir = path.join(PROJECT_DIR, "node_modules", "dep-1", "node_modules", "dep-3");
-        let nan = locateNAN(dir);
+        let nan = locateIncludes(dir, "nan", "nan.h");
         assert.equal(nan, NAN_DIR);
+    });
+
+    it("should locate N-API from nested dependency", function () {
+        let dir = path.join(PROJECT_DIR, "node_modules", "dep-1", "node_modules", "dep-3");
+        let nan = locateIncludes(dir, "node-addon-api", "napi.h");
+        assert.equal(nan, NAPI_DIR);
     });
 
     it("should locate NAN from scoped dependency", function () {
         let dir = path.join(PROJECT_DIR, "node_modules", "@scope", "dep-2");
-        let nan = locateNAN(dir);
+        let nan = locateIncludes(dir, "nan", "nan.h");
         assert.equal(nan, NAN_DIR);
     });
 });
